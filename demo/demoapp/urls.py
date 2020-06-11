2192779fb09ebd1b66d71dc14e6cbda633b78095
Julio Ordonez <julio.ordonez.v@gmail.com>
2020-06-11 13:33:19 -0600

    Refactor code

--- demo/demoapp/urls.py
+++ demo/demoapp/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 
 from demoapp.cruds import Personclass, Countryclass, MenuItemclass
 from demoapp.views import (
-      create_notification_view,
-      color_widget_view,
-      knobView,
-      MyTableView
+    create_notification_view,
+    color_widget_view,
+    knobView,
+    MyTableView
 )
 
 pclss = Personclass()
@@ -14,11 +14,9 @@ countryclss = Countryclass()
 menuclss = MenuItemclass()
 
 urlpatterns = [
-        path('create/notification', create_notification_view),
-        url(r'^markitup/', include('markitup.urls')),
-        path('knobwidget/testform', knobView, name="knobwidgets"),
-        path('colorwidgets', color_widget_view, name="colorwidgets"),
-        path('datatable/person_model_data', MyTableView, name="table_data"),
-
-
-              ] + pclss.get_urls() + countryclss.get_urls() + menuclss.get_urls()
+    path('create/notification', create_notification_view),
+    url(r'^markitup/', include('markitup.urls')),
+    path('knobwidget/testform', knobView, name="knobwidgets"),
+    path('colorwidgets', color_widget_view, name="colorwidgets"),
+    path('datatable/person_model_data', MyTableView, name="table_data"),
+] + pclss.get_urls() + countryclss.get_urls() + menuclss.get_urls()
