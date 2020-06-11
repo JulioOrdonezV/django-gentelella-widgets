c903d4b257d012c861493ecbfbee4c5bfa047677
Julio Ordonez <julio.ordonez.v@gmail.com>
2020-06-11 13:27:36 -0600

    add path to datatable data

--- demo/demoapp/urls.py
+++ demo/demoapp/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url
 from django.urls import path, include
 
 from demoapp.cruds import Personclass, Countryclass, MenuItemclass
-from demoapp.views import create_notification_view, color_widget_view
-from .views import knobView
+from demoapp.views import (
+      create_notification_view,
+      color_widget_view,
+      knobView,
+      MyTableView
+)
 
 pclss = Personclass()
 countryclss = Countryclass()
@@ -14,6 +18,7 @@ urlpatterns = [
         url(r'^markitup/', include('markitup.urls')),
         path('knobwidget/testform', knobView, name="knobwidgets"),
         path('colorwidgets', color_widget_view, name="colorwidgets"),
+        path('datatable/person_model_data', MyTableView, name="table_data"),
 
 
               ] + pclss.get_urls() + countryclss.get_urls() + menuclss.get_urls()
